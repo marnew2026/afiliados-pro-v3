dc993bec12f6c1e4dfdbe9d8050c68973d763ff6
Mariel Santana <marielsantana34@gmail.com>
2026-04-26 22:00:21 -0300

    update campaigns dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,2 @@
 import { campaigns } from "./data/campaigns"
 import CampaignCard from "./components/CampaignCard"
-
-export default function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>📊 Painel Afiliados Pro</h1>
-
-      {campaigns.map((c) => (
-        <CampaignCard key={c.id} campaign={c} />
-      ))}
-    </div>
-  )
-}
