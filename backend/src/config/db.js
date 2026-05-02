66545edf3f2c4b5974ebae1a6499a42affbc2d76
Mariel Santana <marielsantana34@gmail.com>
2026-05-01 21:58:11 -0300

    remove logs sensiveis

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-console.log("DB FILE VERSION 2");
+
 
   try {
     const uri = process.env.MONGO_URL;
 
-    console.log("MONGO_URL existe?", !!uri);
-
-    if (!uri) {
+        if (!uri) {
       throw new Error("MONGO_URL não definida");
     }
 
@@ -16,6 +14,6 @@ console.log("DB FILE VERSION 2");
 
     console.log("🔥 MongoDB conectado");
   } catch (error) {
-    console.log("❌ erro Mongo:", error.message);
+    
   }
 };
