25499015c591d595ca7c1fea464c3fd356f22bd8
Mariel Santana <marielsantana34@gmail.com>
2026-05-17 11:37:12 -0300

    fix schema product

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -6,24 +6,19 @@ const productSchema = new mongoose.Schema(
     nome: String,
     preco: String,
     link: String,
-    imagem: String,affiliateLink: {
-  type: String,
-  default: "",clicks: { type: Number, default: 0 },
-},
+    imagem: String,
+
+    affiliateLink: {
+      type: String,
+      default: "",
+    },
+
+    clicks: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
-const codigo = Math.random().toString(36).substring(2, 10);
-
-  `https://afiliados-pro-v3-2.onrender.com/r/${codigo}`;
-
-
-
-
-
-
-
-
-
 
 export default mongoose.model("Product", productSchema);
