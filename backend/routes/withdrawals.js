c468ab849020e504d0b0bb8c85d9ceaecd80bb76
Mariel Santana <marielsantana34@gmail.com>
2026-05-21 00:50:02 -0300

    debug pix

--- backend/routes/withdrawals.js
+++ backend/routes/withdrawals.js
@@ -3,5 +3,31 @@ import Withdrawal from "../models/Withdrawal.js";
 
 const router = express.Router();
 
+router.post("/create", async (req, res) => {
+  try {
+    console.log("BODY RECEBIDO:", req.body);
 
+    const { userEmail, pixKey, amount } = req.body;
+
+    console.log(userEmail);
+    console.log(pixKey);
+    console.log(amount);
+
+    if (!userEmail || !pixKey || !amount) {
+      return res.json({
+        msg: "dados obrigatorios",
+      });
+    }
+
+    res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      msg: "erro servidor",
+    });
+  }
+});
 export default router;
