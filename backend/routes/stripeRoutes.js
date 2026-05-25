680edf74b35108d8b0679e7dce6ad1ba1371c3e7
Mariel Santana <marielsantana34@gmail.com>
2026-05-24 23:23:19 -0300

    fix stripe route

--- backend/routes/stripeRoutes.js
+++ backend/routes/stripeRoutes.js
@@ -1,74 +1,50 @@
 import express from "express";
 import Stripe from "stripe";
-import User from "../models/User.js";
 
 const router = express.Router();
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-router.post("/create-checkout-session", async (req, res) => {
+router.get("/create-checkout-session", async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
-      mode: "subscription",
+
+      mode: "payment",
+
       line_items: [
         {
-          price: process.env.STRIPE_PRICE_ID,
+          price_data: {
+            currency: "brl",
+
+            product_data: {
+              name: "Plano PRO",
+            },
+
+            unit_amount: 2900,
+          },
+
           quantity: 1,
         },
       ],
-      success_url: "https://afiliados-pro-v3-2.onrender.com/success",
-      cancel_url: "https://afiliados-pro-v3-2.onrender.com/cancel",
+
+      success_url:
+        "https://afiliados-pro-v3-2.onrender.com/success",
+
+      cancel_url:
+        "https://afiliados-pro-v3-2.onrender.com/cancel",
     });
 
-    res.json({ url: session.url });
+    return res.json({
+      url: session.url,
+    });
 
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Erro Stripe" });
-  }
-});
-router.post("/webhook", async (req, res) => {
-  try {
-    console.log("🔥 WEBHOOK RECEBIDO");
-
-    const sig = req.headers["stripe-signature"];
-    console.log("SIGNATURE:", !!sig);
-
-    const event = stripe.webhooks.constructEvent(
-      req.body,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-
-    console.log("EVENTO:", event.type);
-
-    if (event.type === "checkout.session.completed") {
-  const session = event.data.object;
-
-  const email =
-    session.customer_details?.email ||
-    session.customer_email;
-
-  console.log("EMAIL RECEBIDO:", email);
-
-  const usuario = await User.findOne({ email });
-
-  console.log("USUÁRIO ENCONTRADO:", usuario);
-
-  if (email) {
-    await User.findOneAndUpdate(
-      { email },
-      { isPro: true },
-      { new: true }
-    );
-
-    console.log("✅ USUÁRIO VIRou PRO:", email);
-  }
-}
-    res.json({ received: true });
   } catch (err) {
-    console.log("WEBHOOK ERROR:", err.message);
-    res.status(400).send("Webhook Error");
+    console.log(err);
+
+    return res.status(500).json({
+      error: err.message,
+    });
   }
 });
 
