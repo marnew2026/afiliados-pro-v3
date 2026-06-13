9c5330c6b3c0ea4089ea82b270c5c0caa17caf3a
Mariel Santana <marielsantana34@gmail.com>
2026-06-13 14:53:08 -0300

    teste go

--- backend/routes/go.js
+++ backend/routes/go.js
@@ -4,6 +4,11 @@ import Click from "../models/Click.js";
 import Offer from "../models/Offer.js";
 
 const router = express.Router();
+router.get("/:affiliateCode/:offerId", async (req, res) => {
+
+  console.log("🔥🔥🔥 NOVA VERSAO GO 13-06-2026");
+
+  try {
 console.log("🔥 GO ROUTE CARREGADA");
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -26,35 +31,47 @@ router.get("/:affiliateCode/:offerId", async (req, res) => {
     }
 
     const session =
-      await stripe.checkout.sessions.create({
-        mode: "payment",
-        payment_method_types: ["card"],
+    console.log("OFERTA:", offer);
+console.log("NOME:", offer.name);
+console.log("PREÇO:", offer.price);
+console.log("OFERTA:", offer);
+console.log("NOME:", offer.name);
+console.log("PREÇO:", offer.price);
+console.log("TYPE PRICE:", typeof offer.price);
 
-        line_items: [
-          {
-            price_data: {
-              currency: "brl",
-              product_data: {
-                name: offer.name,
-              },
-              unit_amount: offer.price * 100,
-            },
-            quantity: 1,
-          },
-        ],
+const price = Number(offer.price);
 
-        success_url:
-          `${process.env.BASE_URL}/success`,
+console.log("PRICE CONVERTIDA:", price);
+console.log("OFFER COMPLETA:", offer);
+console.log("NAME:", offer.name);
+console.log("PRICE:", offer.price);
+console.log("PRICE NUMBER:", Number(offer.price));
+const session = await stripe.checkout.sessions.create({
+  mode: "payment",
+  payment_method_types: ["card"],
 
-        cancel_url:
-          `${process.env.BASE_URL}/cancel`,
-
-        metadata: {
-          affiliateId: affiliateCode,
-          offerId,
-          commission: offer.commissionPercent,
+  line_items: [
+    {
+      price_data: {
+        currency: "brl",
+        product_data: {
+          name: offer.name,
         },
-      });
+        unit_amount: Math.round(price * 100),
+      },
+      quantity: 1,
+    },
+  ],
+
+  success_url: `${process.env.BASE_URL}/success`,
+  cancel_url: `${process.env.BASE_URL}/cancel`,
+
+  metadata: {
+    affiliateId: affiliateCode,
+    offerId,
+    commission: offer.commissionPercent,
+  },
+});
 
     return res.redirect(session.url);
 
